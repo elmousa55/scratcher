7ad1c896079a451096a24a094ed36dbddcb0ade9
El Mousa <elmousa33333@gmail.com>
2024-06-19 13:21:10 -0400

    Fix port binding for Heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -15,4 +16,5 @@ def receive_stock_data():
     return jsonify({"status": "success", "data": data}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
